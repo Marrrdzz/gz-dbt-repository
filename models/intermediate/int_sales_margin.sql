a5e2c20950d70ec009e73738a52f1dc5307a867b
Marie <perocheaumarie@gmail.com>
2025-11-04 14:40:24 +0000

    margin and cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,18 @@
+WITH finance as (
+SELECT 
+sales.date_date
+,sales.orders_id
+,sales.revenue
+,sales.quantity
+,p.purchase_price
+,p.products_id
+FROM {{ ref('stg_raw__sales') }} AS sales
+LEFT JOIN {{ ref('stg_raw__product') }} AS p
+USING (products_id)
+)
+
+SELECT 
+*
+,ROUND((purchase_price*quantity),2) as purchase_cost
+,ROUND((revenue - ROUND((purchase_price*quantity),2)),2) as margin
+FROM finance
