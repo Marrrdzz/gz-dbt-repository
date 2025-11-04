fd7e6f433a1ff7b738eed6509fbc93f42ed27ae8
Marie <perocheaumarie@gmail.com>
2025-11-04 15:52:07 +0000

    operational_margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,9 @@
+SELECT 
+date_date
+,orders_id
+,quantity
+,revenue
+,ROUND((margin+shipping_fee-logcost-ship_cost),2) as operational_margin
+FROM {{ ref('int_orders_margin') }} AS orders_margin
+LEFT JOIN {{ ref('stg_raw__ship') }} AS ship
+USING(orders_id)
